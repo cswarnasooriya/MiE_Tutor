477db3efe42c4474279000daa5ae348d65470ca5
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-07-04 17:41:26 +0530

    Home section updated

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import '../CSS/Home.css';
+
+const Home = () => {
+  return (
+    <div className="hero">
+
+      <div className="hero-content">
+        <h1 className="hero-text">Online Courses</h1>
+        <p className="hero-des">
+          Browse and follow the collection of our best and modern interesting courses and lectures anywhere and any time with your favourites and so on.
+        </p>
+      </div>
+
+      <div className="hero-img"></div>
+      
+    </div>
+  )
+}
+
+export default Home
